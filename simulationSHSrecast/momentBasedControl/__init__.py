b34dbff3442b01e55f3cc766f2a150aec4f6fa1f
Khem Raj Ghusinga <khemrajghusinga@gmail.com>
2018-11-08 21:26:28 -0500

    removed stuff that's not reqd for simulations

--- simulationSHSrecast/momentBasedControl/__init__.py
+++ simulationSHSrecast/momentBasedControl/__init__.py
@@ -1,5 +1,3 @@
-from poly import constant,vararray,polyarr,var,cos,sin,dot,outer,hstack, \
-    functify,monomialString,subs,proj,jacobian,MonomialsUpTo,residualMonomials
-from momentBasedControl import jumpDiffusion,objective,GLTime,sdpBounds, \
-    Minimize,Maximize,Both,randSolve,Euler,Pseudospectral,LagInterp,generator, \
-    maximalState,maximalEqualityConstraint
+from poly import constant,vararray,polyarr,var,dot,outer,hstack, \
+    functify,subs,proj,jacobian
+from momentBasedControl import jumpDiffusion
